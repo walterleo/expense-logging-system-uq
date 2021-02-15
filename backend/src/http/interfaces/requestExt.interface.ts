27b369222413745bdecd0a4dce8985b44cf91184
walterleo <meza.walter@gmail.com>
2021-02-14 21:36:21 -0500

    fix type errors

--- backend/src/http/interfaces/requestExt.interface.ts
+++ backend/src/http/interfaces/requestExt.interface.ts
@@ -2,5 +2,5 @@ import { Request } from 'express';
 import {QueryParamsParsedInterface} from "./queryParamsParsed.interface";
 
 export interface RequestExtInterface extends Request {
-    queryParamsParsed: QueryParamsParsedInterface;
+    queryParamsParsed?: QueryParamsParsedInterface;
 };
